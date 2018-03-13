36b87636e9ab35c59a226b20c66e0436a681575e
Tester-JasonWang <wxy_890316@163.com>
2018-03-13 18:43:56 +0800

    update

--- djproject/urls.py
+++ djproject/urls.py
@@ -20,15 +20,4 @@ from demo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
-    path('current_time/', views.current_time),
-    path('', views.index),
-    path('get_names/', views.get_names),
-    re_path('hello/', views.hello),
-    re_path('^test/([0-9]{1,2})/$', views.test),  # 正则表达式用括号括起，表示将匹配到的值作为参数传递给views.text()方法
-    path('notice/', views.notice),
-    path('display_meta/', views.display_meta),
-    path('search_form/', views.search_form),
-    path('search_result/', views.search_result),
-    path('cases/', views.get_case_list)
 ]
